b80445e1a18cb49e1eaca50167a2ce784f42651e
Jiaxi <dev.jiaxiwu@gmail.com>
2020-05-23 18:55:47 -0400

    committing query

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 
 class Search extends React.Component {
   constructor(props) {
@@ -10,20 +11,24 @@ class Search extends React.Component {
       message: "",
     };
   }
-
+    fetchSearchResults = ( updatedPageNo, query) => {
+        const searchUrl = `https://api.edamam.com/api/food-database/parser?ingr=${query}&app_id=4f903669&app_key=d13ce675ba01b3c47f4d949d2a628c33`
+    }
     handleOnInputChange = ( event ) => {
         const query = event.target.value;
-        this.setState ({query: query})
+        this.setState ({query: query, loading: true, message: ''})
     };
 
   render() {
+      const {query} = this.state;
     return (
       <div className="container">
         <h2> Calorie Counter </h2>
         <label className="search-label" htmlFor="search-inout">
           <input
             type="text"
-            value=""
+            name="query"
+            value={query}
             id="search input"
             placeholder="Search..."
             onChange={this.handleOnInputChange}
