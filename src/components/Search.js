52fdf873ee4464d770ab9e6aa0fdf0a0e22f9198
Jiaxi <dev.jiaxiwu@gmail.com>
2020-05-23 20:41:52 -0400

    renderSearch

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,76 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: '',
+      query: "",
       results: {},
       loading: false,
       message: "",
     };
+    this.cancel = "";
   }
-    fetchSearchResults = ( updatedPageNo, query) => {
-        const searchUrl = `https://api.edamam.com/api/food-database/parser?ingr=${query}&app_id=4f903669&app_key=d13ce675ba01b3c47f4d949d2a628c33`
+  fetchSearchResults = (query) => {
+    const searchUrl = `https://api.edamam.com/api/food-database/parser?ingr=${query}&app_id=4f903669&app_key=d13ce675ba01b3c47f4d949d2a628c33`;
+
+    if (this.cancel) {
+      this.cancel.cancel();
     }
-    handleOnInputChange = ( event ) => {
-        const query = event.target.value;
-        this.setState ({query: query, loading: true, message: ''})
-    };
+
+    this.cancel = axios.CancelToken.source();
+
+    axios
+      .get(searchUrl, {
+        cancelToken: this.cancel.token
+      })
+      .then((res) => {
+          const resultNotFound = ! res.data.hints.length
+                                ? 'There are no more search results'
+                                : '';
+            this.setState({results: res.data.hints,
+                            message: resultNotFound,
+                            loading: false
+        })
+      })
+      .catch((error) => {
+        if (axios.isCancel(error) || error) {
+          this.setState({
+            loading: false,
+            message: "Failed to fetch the data",
+          });
+        }
+      });
+  };
+
+  handleOnInputChange = (event) => {
+    const query = event.target.value;
+    this.setState({ query: query, loading: true, message: "" }, () => {
+      this.fetchSearchResults(query);
+    });
+  };
+
+  renderSearchResults = () => {
+      const { results } = this.state;
+
+      if ( Object.keys( results ).length && results.length ) {
+          return (
+              <div className="results-container">
+                { results.map( result => {
+                    return(
+                        <a key={result.foodId} href={result.image} className="result-item">
+                            <h6 className="image-username">{result.username}</h6>
+                        </a>
+                    )
+                })}
+              </div>
+          )
+      }
+  }
 
   render() {
-      const {query} = this.state;
+    const { query } = this.state;
     return (
       <div className="container">
         <h2> Calorie Counter </h2>
@@ -39,4 +89,4 @@ class Search extends React.Component {
   }
 }
 
-export default Search
+export default Search;
