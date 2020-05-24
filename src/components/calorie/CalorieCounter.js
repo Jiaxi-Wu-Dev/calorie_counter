fb55a3991f67b661c53b7061d1654eaa9d7caab2
Jiaxi <dev.jiaxiwu@gmail.com>
2020-05-24 17:41:54 -0400

    handleusername

--- src/components/calorie/CalorieCounter.js
+++ src/components/calorie/CalorieCounter.js
@@ -1,7 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CalorieCounter extends Component {
-    render(){
-        return()
-    }
+  render() {
+    return (
+      <div className="CalorieCounter">
+        My Calorie Counter
+        <LoginComponent />
+      </div>
+    );
+  }
 }
+
+class LoginComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "username",
+      password: "",
+    };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handleUsernameChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  handlePasswordChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      password: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="LoginComponent">
+        User Name:{" "}
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleUsernameChange}
+        />
+        Password:{" "}
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+        <button>Login</button>
+      </div>
+    );
+  }
+}
+
+export default CalorieCounter;
